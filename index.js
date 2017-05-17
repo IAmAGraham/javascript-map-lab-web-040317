6f533c4aa9351818aa87ab3189abb3a2aa27eb4d
github username <github email address>
2017-05-17 17:35:28 -0400

    Done.

--- index.js
+++ index.js
@@ -21,3 +21,15 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+const sortedRobots = robots.map(function (robot) {
+  const iAmADecepticon = knownDecepticons.includes(robot.name)
+    return Object.assign({}, robot, {
+      alliance: iAmADecepticon ? 'decepticon' : 'autobot'
+    });
+  });
+
+const coloredZebraStripes = zebraStripes.map(function (stripe, index){
+  return Object.assign({}, stripe, { color: (index % 2 === 0)  ? 'black' : 'white'
+});
+});
